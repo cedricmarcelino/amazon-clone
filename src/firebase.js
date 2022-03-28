890d1fa76cdde12df84e0338e1609c3c608b231b
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2022-03-28 20:25:28 +0800

    Cleaned up unnecesarry files and initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBX0Ay837IUOIPPTjJwmP2s6yrYJSCjRMs",
+    authDomain: "clone-9b8e7.firebaseapp.com",
+    projectId: "clone-9b8e7",
+    storageBucket: "clone-9b8e7.appspot.com",
+    messagingSenderId: "702840810048",
+    appId: "1:702840810048:web:995d2135b7843ef0c60da5"
+  };
