cd1554e259714b6511609346156bd2feec12ea84
Cedric Marcelino <cedricmarcelino1105@gmail.com>
2022-03-28 21:24:32 +0800

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
-      {/* Header */}
+      <Header />
       {/* Home */}
     </div>
   );
